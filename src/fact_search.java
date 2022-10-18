f9160cf4133cd40ac56ba3987e5dbfc5aecb3624
Azat <azat.takhautdinov@gmail.com>
2022-10-18 23:23:10 +0500

    Updated factorial calculator, after studying the topic "Recursion".

--- src/fact_search.java
+++ src/fact_search.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class fact_search {
     public static void main (String []args) {
         Scanner scan = new Scanner(System.in);
-        int scan1 = scan.nextInt();
-        int fact = 1;
-
-        while (scan1>0){
-            fact=fact*scan1--;
-        }
-        System.out.println(fact);
-
+        System.out.println(fac(scan.nextInt()));
     }
+    private static int fac(int n){
+        if(n==1)
+            return 1;
+        return n *fac(n-1);
+    }
+
+
 }
